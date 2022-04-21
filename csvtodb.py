9c9909ca360e25c3cbb548dd053f8b8bb6b50122
Wambugu-Muchemi <wambugumuchemi2021@gmail.com>
2022-04-21 18:31:33 +0300

    Removed sensitive information by implimenting dotenv.

--- csvtodb.py
+++ csvtodb.py
@@ -7,16 +7,28 @@ from unicodedata import name
 import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
+from dotenv import load_dotenv
+import os
 
-url = "https://account.africastalking.com/api/v1/apps/jndydujron/sms/bulk/outbox?page=0&count=100"
+from test_auto_auth import COOKIE
+
+load_dotenv()
+URL2 = os.getenv("URL2")
+TOKEN = os.getenv("TOKEN")
+CLIENT_ID = os.getenv("CLIENT_ID")
+USER_AGENT = os.getenv("USER_AGENT")
+COOKIE2 = os.getenv("COOKIE2")
+
+
+url = URL2
 
 payload={}
 headers = {
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTY0MjYsImZpcnN0TmFtZSI6IkRhbm5pZSIsImxhc3ROYW1lIjoiTGFicyIsImVtYWlsIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImNvdW50cnkiOiJLZW55YSIsImN1cnJlbmN5IjoiS0VTIiwidGltZU9mZnNldCI6NzIwLCJhY3RpdmF0ZWQiOnRydWUsInJvbGUiOiJub3JtYWwiLCJpbnRlcmNvbSI6eyJ1c2VyX2hhc2giOiJhMzJkMDg4ODk5YTFmZDYyZDZjNTg0MDA1MzBkMDI1MDI5MzIwNTg3OWI2N2UwNGU4ODA0Zjk1NzFlNDA5ZmQyIn0sInNsYWNrIjpudWxsLCJsb2NhbGUiOiJISyIsImNsaWVudElkIjoibmVzdC5hY2NvdW50LmRhc2hib2FyZCIsImlhdCI6MTY1MDUyMTYzMSwiZXhwIjoxNjUwNTI1MjMxLCJpc3MiOiJodHRwczovL2FjY291bnQuYWZyaWNhc3RhbGtpbmcuY29tIiwic3ViIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImp0aSI6IjYyNjBmNjFlOTg3ZTE3OTBmMDExZjIwZSJ9.4mg81S8k8OiByHQYEQpoKQeiONl0J4G5cVz0ILM2Y4M',
-  'X-Client-Id': 'nest.account.dashboard',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+  'Authorization': TOKEN,
+  'X-Client-Id': CLIENT_ID,
+  'User-Agent': USER_AGENT,
   'Accept': '*/*',
-  'Cookie': 'nalb=http://10.222.105.54:8080'
+  'Cookie': COOKIE2
 }
 
 
--- test_auto_auth.py
+++ test_auto_auth.py
@@ -1,11 +1,21 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
-url = "https://account.africastalking.com/api/v1/auth/signin"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+COOKIE = os.getenv("COOKIE")
+URL = os.getenv("URL")
+
+url = URL
 
 payload = json.dumps({
-  "email": "dmain5054@gmail.com",
-  "password": "YDkaft@DVc7fVyH",
+  "email": EMAIL,
+  "password": PASSWORD,
   "timeOffset": 720
 })
 headers = {
@@ -14,7 +24,7 @@ headers = {
   'Content-Type': 'application/json',
   'Accept': '*/*',
   'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTY0MjYsImZpcnN0TmFtZSI6IkRhbm5pZSIsImxhc3ROYW1lIjoiTGFicyIsImVtYWlsIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImNvdW50cnkiOiJLZW55YSIsImN1cnJlbmN5IjoiS0VTIiwidGltZU9mZnNldCI6NzIwLCJhY3RpdmF0ZWQiOnRydWUsInJvbGUiOiJub3JtYWwiLCJpbnRlcmNvbSI6eyJ1c2VyX2hhc2giOiJhMzJkMDg4ODk5YTFmZDYyZDZjNTg0MDA1MzBkMDI1MDI5MzIwNTg3OWI2N2UwNGU4ODA0Zjk1NzFlNDA5ZmQyIn0sInNsYWNrIjpudWxsLCJsb2NhbGUiOiJISyIsImNsaWVudElkIjoibmVzdC5hY2NvdW50LmRhc2hib2FyZCIsImlhdCI6MTYyMzA2ODM4MywiZXhwIjoxNjIzMDcxOTgzLCJpc3MiOiJodHRwczovL2FjY291bnQuYWZyaWNhc3RhbGtpbmcuY29tIiwic3ViIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImp0aSI6IjYwYmUwZWRmNjU3ZGE2ZDVhMTE2ZDNiMCJ9.RQNeZgcwRhAhl80NvQ1VV_PteGW1dioh53fioeZFn8g',
-  'Cookie': 'nalb=http://10.222.105.54:8080'
+  'Cookie': COOKIE
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
