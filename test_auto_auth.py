993d175d9c79dd0ecc82a015d91cea9dd9b06c62
Wambugu-Muchemi <wambugumuchemi2021@gmail.com>
2022-04-21 19:45:24 +0300

    Moved sensitive data .env

--- test_auto_auth.py
+++ test_auto_auth.py
@@ -10,6 +10,9 @@ EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 COOKIE = os.getenv("COOKIE")
 URL = os.getenv("URL")
+TOKEN2 = os.getenv("TOKEN2")
+CLIENT_ID = os.getenv("CLIENT_ID")
+USER_AGENT = os.getenv("USER_AGENT")
 
 url = URL
 
@@ -19,11 +22,11 @@ payload = json.dumps({
   "timeOffset": 720
 })
 headers = {
-  'X-Client-Id': 'nest.account.dashboard',
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+  'X-Client-Id': CLIENT_ID,
+  'User-Agent': USER_AGENT,
   'Content-Type': 'application/json',
   'Accept': '*/*',
-  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTY0MjYsImZpcnN0TmFtZSI6IkRhbm5pZSIsImxhc3ROYW1lIjoiTGFicyIsImVtYWlsIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImNvdW50cnkiOiJLZW55YSIsImN1cnJlbmN5IjoiS0VTIiwidGltZU9mZnNldCI6NzIwLCJhY3RpdmF0ZWQiOnRydWUsInJvbGUiOiJub3JtYWwiLCJpbnRlcmNvbSI6eyJ1c2VyX2hhc2giOiJhMzJkMDg4ODk5YTFmZDYyZDZjNTg0MDA1MzBkMDI1MDI5MzIwNTg3OWI2N2UwNGU4ODA0Zjk1NzFlNDA5ZmQyIn0sInNsYWNrIjpudWxsLCJsb2NhbGUiOiJISyIsImNsaWVudElkIjoibmVzdC5hY2NvdW50LmRhc2hib2FyZCIsImlhdCI6MTYyMzA2ODM4MywiZXhwIjoxNjIzMDcxOTgzLCJpc3MiOiJodHRwczovL2FjY291bnQuYWZyaWNhc3RhbGtpbmcuY29tIiwic3ViIjoiZG1haW41MDU0QGdtYWlsLmNvbSIsImp0aSI6IjYwYmUwZWRmNjU3ZGE2ZDVhMTE2ZDNiMCJ9.RQNeZgcwRhAhl80NvQ1VV_PteGW1dioh53fioeZFn8g',
+  'Authorization': TOKEN2,
   'Cookie': COOKIE
 }
 
