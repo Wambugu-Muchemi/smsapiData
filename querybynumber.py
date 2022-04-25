053fb3ade4c66f186a2b48998aae284c1ea9d2da
Wambugu-Muchemi <wambugumuchemi2021@gmail.com>
2022-04-25 10:45:53 +0300

    Script can now query the latest 10 messages for Sub ID or SMS code

--- querybynumber.py
+++ querybynumber.py
@@ -7,7 +7,7 @@ cleanedphone = f"%2B254{phone}"
 f"{cleanedphone}"
 with open('.bear', 'r') as f:
     accesstoken = f.read()
-url = f"https://account.africastalking.com/api/v1/apps/jndydujron/sms/bulk/outbox?filter=recipient&value={cleanedphone}&page=0&count=3"
+url = f"https://account.africastalking.com/api/v1/apps/jndydujron/sms/bulk/outbox?filter=recipient&value={cleanedphone}&page=0&count=10"
 
 payload='from=1-1-2022'
 headers = {
@@ -27,6 +27,14 @@ message_dict = response.json()['data']
 message1 = message_dict[0]['message']
 message2 = message_dict[1]['message']
 message3 = message_dict[2]['message']
+message4 = message_dict[3]['message']
+message5 = message_dict[4]['message']
+message6 = message_dict[5]['message']
+message7 = message_dict[6]['message']
+message8 = message_dict[7]['message']
+message9 = message_dict[8]['message']
+message10 = message_dict[9]['message']
+
 # print('\n1. '+message1)
 # print('\n2. '+message2)
 # print('\n3. '+message3)
@@ -39,6 +47,20 @@ if selection:=1:
         print(message2)
     elif 'subscription' in message3:
         print(message3)
+    elif 'subscription' in message4:
+        print(message4)
+    elif 'subscription' in message5:
+        print(message5)
+    elif 'subscription' in message6:
+        print(message6)
+    elif 'subscription' in message7:
+        print(message7)  
+    elif 'subscription' in message8:
+        print(message8)
+    elif 'subscription' in message9:
+        print(message9)     
+    elif 'subscription' in message10:
+        print(message10)             
     else:
         print('No Sub ID message found. Please recheck number or reach out to your regional technical support for help.')
       
@@ -49,6 +71,20 @@ elif selection:=2:
         print(message2)
     elif 'verification' in message3:
         print(message3)
+    elif 'verification' in message4:
+        print(message4)
+    elif 'verification' in message5:
+        print(message5)
+    elif 'verification' in message6:
+        print(message6)
+    elif 'verification' in message7:
+        print(message7)  
+    elif 'verification' in message8:
+        print(message8)
+    elif 'verification' in message9:
+        print(message9)     
+    elif 'verification' in message10:
+        print(message10)    
     else:
         print('No SMS code message found. Please recheck number or reach out to your regional technical support for help.')
 
